0b399a60d80c7c7b7c6e0ffda0f39c6d0cec2ca1
Gregor Urban <urbgreg1@gmail.com>
2020-08-26 22:28:19 -0700

    Python3 compatibility and minor code clean-up

--- deep3Dtrain.py
+++ deep3Dtrain.py
@@ -27,7 +27,7 @@ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABI
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
-
+from __future__ import print_function
 import argparse
 import utils.data_and_CV_handler as data_and_CV_handler
 import numpy as np
@@ -61,18 +61,13 @@ def train(list_training_data, list_training_labels, list_test_data,
         learning_rate:
             float, initial learning rate used for training
 
-
     """
     import utils.Segmentation_trainer as Segmentation_trainer
-    
     assert len(list_training_data) == len(list_training_labels), "The total number of data files and label files differs: "+str(len(list_training_data))+' vs '+str(len(list_training_labels))
     autosave_frequency_minutes=60 # saves a copy of the CNN's parameters on the disk every X minutes
     autosave_n_files=3
-
-
     # if anything weird happens during training (e.g. loss increases by more than a factor of two), try a lower value
     Initial_learning_rate = learning_rate
-
     # probably no need to change these values:
     momentum = 0.9
     patch_depth = 1
@@ -84,13 +79,9 @@ def train(list_training_data, list_training_labels, list_test_data,
     gradient_clipping       = 0
     bWeightDecay            = 0
     bDropoutEnabled         = 0
-    
-    
     n_labels_pred_per_dim = 5 # was 4
-
     # number of classes in the data set - e.g. 2 means binary classification.
     n_classes=2
-
     # This is where the CNN is specified:
     #
     # the first value in each of the lists corresponds to the first 3D-convolutional layer, the second value(s) to the second layer, etc.
@@ -105,20 +96,15 @@ def train(list_training_data, list_training_labels, list_test_data,
     
     nnet_args["nof_filters"] = [int(np.ceil(network_size_factor * x)) for x in nnet_args["nof_filters"][:-1]] + [nnet_args["nof_filters"][-1]]
         
-
     num_patches_per_batch = 4  # a better setting is e.g. 4 if you have enough GPU memory or use the CPU, otherwise try 2
     
     input_to_cnn_depth = patch_depth #use 2 if you enable the pseudo-recursion
-
-
 
     override_data_set_filenames = {"train_data":list_training_data,
                                    "test_data":list_test_data,
                                    "train_labels":list_training_labels}
 
     n_labels_per_batch = n_labels_pred_per_dim**(3)
-
-
 
     cnn, patchCreator = Segmentation_trainer.Build3D(nnet_args, n_labels_per_batch=n_labels_per_batch, notrain= False,
                                      bDropoutEnabled = bDropoutEnabled,
@@ -136,22 +122,18 @@ def train(list_training_data, list_training_labels, list_test_data,
 
     cnn.randomize_weights(scale_w = init_scale_factor)
 
-
     if load_previous_save:
         try:
             cnn.LoadParameters(save_name+"/end_"+str(save_name)+".save")
-            print 'load_previous_save:: found and loaded saved parameters'
+            print('load_previous_save:: found and loaded saved parameters')
         except:
-            print 'load_previous_save:: found no saved parameters or CNN is incompatible'
+            print('load_previous_save:: found no saved parameters or CNN is incompatible')
     Segmentation_trainer.train_net(cnn, patchCreator, num_patches_per_batch=num_patches_per_batch,
                                    LR_start=Initial_learning_rate, momentum=momentum,
                                    save_name=save_name, b_no_test_set=True, b_use_ext_error=0,
                                    autosave_frequency_minutes=autosave_frequency_minutes,
                                    autosave_n_files=autosave_n_files,
                                    b_use_data_augmentation = b_use_data_augmentation)
-
-
-
 
 
 def findall(paths):
@@ -167,26 +149,19 @@ def tolist(x):
 
 def main():
     parser = argparse.ArgumentParser(description='Main module to train a 3D-CNN for segmentation')
-    
     parser.add_argument('-data', required=True, type=str, nargs='+', help='Any number and combination of paths to files or folders that will be used as input-data for training the CNN')
     parser.add_argument('-labels', required=True, type=str, nargs='+', help='Any number and combination of paths to files or folders that will be used as target for training the CNN (values must be 0/1)')
-    
     parser.add_argument('-lr', type=float, default=1e-5,  help='initial learning rate (step size) for training the CNN')
     parser.add_argument('-name', default='deep3Dtrain_model_1', type=str,  help='name of the model (affects filenames) -- specify the same name when using deep3Dtest.py')
     parser.add_argument('-convert_labels', default=1, type=int, nargs=1, help='if labels are not binary: this will convert values >1 to 1')
-    
     parser.add_argument('-data_clip_range', default=None, type=float, nargs=2, help='[Mostly for single-channel data] Clip all pixel-values outside of the given range (important if values of volumes have very different ranges!)')
-    
     parser.add_argument('-CNN_width_scale', default=1., type=float, help='Scale factor for the layer widths of the CNN; values larger than 1 will increase the total network size beyond the default size, but be careful to not exceed your GPU memory.')
-    
-    
     args = parser.parse_args()
     
     #print args
     data = findall(tolist(args.data))
     labels = findall(tolist(args.labels))
     load_previous_save = False
-
 
     train(list_training_data=data,    
          list_training_labels=labels,    
@@ -200,20 +175,6 @@ def main():
 
 
 
-
-
-
-
-
 if __name__ == '__main__':
     main()
     
-
-
-
-
-
-
-
-
-
